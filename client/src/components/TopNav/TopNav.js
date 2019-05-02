ac78e7b98278dd14e2cb03a63d0b2e27a77b22a2
haleylanebrown <47645417+haleylanebrown@users.noreply.github.com>
2019-05-02 15:04:25 -0600

    home changes

--- client/src/components/TopNav/TopNav.js
+++ client/src/components/TopNav/TopNav.js
@@ -57,7 +57,7 @@ export default class Navigation extends Component {
         return (
             <div>
     
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg navbar-light bg-white">
                 <a className="navbar-brand" href="#"><img src="./assets/images/MeetInTheMiddleSmall.png" alt="logo" height="40px" /></a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
