b9e1c7ec9fb2b884c46b8bb9ad081d522620409c
ksknight <korvette_00Whotmail.com>
2019-05-08 14:57:25 -0600

    updated marker

--- client/src/components/Map/MapWithAMarker.js
+++ client/src/components/Map/MapWithAMarker.js
@@ -5,36 +5,12 @@ import "./style.css"
 
 
 const MapWithAMarker = compose(withScriptjs, withGoogleMap)(props => {
-    // console.log("Map with a marker props.markers: ", props.placeID)
-    return (
-      <GoogleMap defaultZoom={10} defaultCenter={{ lat: 39.7392, lng: -104.9903 }} center={props.currentLocation}>
-        {/* {console.log(typeof(props.currentLocation))} */}
-        {/* {console.log(`default loc ${defaultCenter}`)} */}
-        {console.log("CURRENT LOCATION",props.currentLocation)}
-
-        {props.markers.map(marker => {
-          
-          const onClick = props.onClick.bind(this, marker)
-          return (
-            <Marker
-              key={marker.place.id}
-              onClick={() => onClick(marker.id)}
-              id={marker.id}
-              position={{ lat: marker.place.lat, lng: marker.place.lng }}
-            >
-              {props.selectedMarker === marker &&
-                <InfoWindow>
-                  <div>
-                    {marker}
-                  </div>
-                </InfoWindow>}
-  
-            </Marker>
-          )
-        })}
-      </GoogleMap>
-    )
-  })
+  // console.log("Map with a marker props.markers: ", props.placeID)
+  return (
+    <GoogleMap defaultZoom={10} defaultCenter={{ lat: 39.7392, lng: -104.9903 }} center={props.currentLocation}>
+      {/* {console.log(typeof(props.currentLocation))} */}
+      {/* {console.log(`default loc ${defaultCenter}`)} */}
+      {console.log("CURRENT LOCATION", props.currentLocation)}
 
       {props.markers.map(marker => {
 
@@ -52,10 +28,10 @@ const MapWithAMarker = compose(withScriptjs, withGoogleMap)(props => {
                   {marker}
                 </div>
               </InfoWindow>}
+
           </Marker>
         )
       })}
-
       <Marker
         icon={{ url: "http://chart.googleapis.com/chart?chst=d_map_pin_letter&chld=xxx%7c5680FC%7c000000&.png", }}
         key={props.add1.lat}
@@ -65,22 +41,22 @@ const MapWithAMarker = compose(withScriptjs, withGoogleMap)(props => {
         icon={{ url: "http://chart.googleapis.com/chart?chst=d_map_pin_letter&chld=xxx%7c5680FC%7c000000&.png", }}
         key={props.add2.lat}
         position={{ lat: props.add2.lat, lng: props.add2.lng }}>
-        </Marker>
+      </Marker>
       <Marker
         icon={{ url: "http://chart.googleapis.com/chart?chst=d_map_pin_letter&chld=xxx%7c5680FC%7c000000&.png", }}
         key={props.add3.lat}
         position={{ lat: props.add3.lat, lng: props.add3.lng }}>
-        </Marker>
+      </Marker>
       <Marker
         icon={{ url: "http://chart.googleapis.com/chart?chst=d_map_pin_letter&chld=xxx%7c5680FC%7c000000&.png", }}
         key={props.add4.lat}
         position={{ lat: props.add4.lat, lng: props.add4.lng }}>
-        </Marker>
+      </Marker>
       <Marker
         icon={{ url: "http://chart.googleapis.com/chart?chst=d_map_pin_letter&chld=xxx%7c5680FC%7c000000&.png", }}
         key={props.add5.lat}
         position={{ lat: props.add5.lat, lng: props.add5.lng }}>
-        </Marker>
+      </Marker>
     </GoogleMap>
   )
 })
