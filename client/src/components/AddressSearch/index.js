19bd47a61ad038c2a2239a580612ebb110ecfa01
ksknight <korvette_00Whotmail.com>
2019-05-08 09:59:57 -0600

    removed address drop down when not logged in

--- client/src/components/AddressSearch/index.js
+++ client/src/components/AddressSearch/index.js
@@ -4,50 +4,70 @@ import ReactGooglePlacesSuggest from "react-google-places-suggest"
 import API from "../../utils/API"
 import "./style.css"
 const MY_API_KEY = "AIzaSyCONkF6ans7kgeS5x--mxwLeMmH0aNJ3vE" // fake
- 
+
 export default class GoogleSuggest extends Component {
     state = {
         search: "",
         value: "",
-        userAddress: []
+        userAddress: [],
+        loggedIn: false,
+        userID: ""
     }
     constructor(props) {
         super(props);
-      }
- 
+    }
+    componentDidMount() {
+        this.loggedIn();
+    }
+
+    loggedIn = () => {
+        API.isLoggedIn().then(user => {
+            console.log(user)
+            if (user.data.loggedIn) {
+                this.setState({
+                    loggedIn: true,
+                    username: user.data.user._id
+                });
+                console.log(`User: data ${user.data}`);
+            }
+        }).catch(err => {
+            console.log("Not logged in");
+        });
+    }
+
     handleInputChange = e => {
-        this.setState({search: e.target.value, value: e.target.value})
+        this.setState({ search: e.target.value, value: e.target.value })
         this.props.update(this.props.num, this.state.value)
     }
- 
+
     handleSelectSuggest = (geocodedPrediction, originalPrediction) => {
         // console.log(`suggested predictions:  \n${geocodedPrediction}\n\n ${originalPrediction}`) // eslint-disable-line
         this.props.update(this.props.num, geocodedPrediction.formatted_address);
-        this.setState({search: "", value: geocodedPrediction.formatted_address})
+        this.setState({ search: "", value: geocodedPrediction.formatted_address })
         this.props.coords(this.props.num, this.state.value);
 
     }
     postAddress = () => {
 
-        API.postAddress(this.state.userId, {address: this.state.value})
-    
-          .then(res => console.log("post: please"))
-          .catch(err => console.log("post: no"))
+        API.postAddress(this.state.userId, { address: this.state.value })
+
+            .then(res => console.log("post: please"))
+            .catch(err => console.log("post: no"))
     }
 
     findAll = () => {
         // console.log(`${this.props.num}'s search bar hit`)
         API.getAll(this.state.userId)
-        .then(res=> {
-            let addressArray = [];
-            for (let i =0; i < res.data.length; i++) {
-                addressArray.push(res.data[i].address)
-            }
-            this.setState({userAddress: addressArray})
-        })
-        .catch(err=> console.log(err))
+            .then(res => {
+                let addressArray = [];
+                for (let i = 0; i < res.data.length; i++) {
+                    addressArray.push(res.data[i].address)
+                }
+                this.setState({ userAddress: addressArray })
+            })
+            .catch(err => console.log(err))
     }
-    
+
     handleNoResult = () => {
         console.log('No results for ', this.state.search)
     }
@@ -55,7 +75,7 @@ export default class GoogleSuggest extends Component {
     selectAddress = (event) => {
         // event.preventDefault();
         const value = event.target.value;
-        this.setState({search: value, value: value})
+        this.setState({ search: value, value: value })
     }
 
     componentDidMount() {
@@ -73,12 +93,12 @@ export default class GoogleSuggest extends Component {
             }
         })
             .catch(err => {
-            console.log(err);
-        });
+                console.log(err);
+            });
     }
- 
+
     render() {
-        const {search, value} = this.state
+        const { search, value } = this.state
         return (
             <ReactGoogleMapLoader
                 params={{
@@ -88,48 +108,49 @@ export default class GoogleSuggest extends Component {
                 render={googleMaps =>
                     googleMaps && (
                         <div className="input-bar">
-                        <div className="input-bar-item">
-                        <ReactGooglePlacesSuggest
-                            googleMaps={googleMaps}
-                            autocompletionRequest={{
-                                input: search,
-                                // Optional options
-                                // https://developers.google.com/maps/documentation/javascript/reference?hl=fr#AutocompletionRequest
-                            }}
-                            // Optional props
-                            onNoResult={this.handleNoResult}
-                            onSelectSuggest={this.handleSelectSuggest}
-                            textNoResults="My custom no results text" // null or "" if you want to disable the no results item
-                            customRender={prediction => (
-                                <div className="customWrapper input-group">
-                                    {prediction
-                                        ? prediction.description
-                                        : "My custom no results text"}
-                                </div>
-                            )}
-                        >
-                            <input
-                                className= "form-control width100"
-                                type="text"
-                                value={value}
-                                placeholder="Search a location"
-                                onChange={this.handleInputChange}
-                            />
-                        </ReactGooglePlacesSuggest>
-                        <span className="input-group-btn">
-                        <button type="button" className="btn btn-warning" onClick={this.postAddress}>Save</button>
-                    {/* <button type="button" className="btn btn-info">Select from Profile</button> */}
-                    <select onChange={this.selectAddress} name="saved">
-                        {this.state.userAddress.map(address=> {
-                            return (<option value={address}>{address}</option>)
-                        })}
-                    </select>
-                    </span>
-                </div>
-                </div>
+                            <div className="input-bar-item">
+                                <ReactGooglePlacesSuggest
+                                    googleMaps={googleMaps}
+                                    autocompletionRequest={{
+                                        input: search,
+                                        // Optional options
+                                        // https://developers.google.com/maps/documentation/javascript/reference?hl=fr#AutocompletionRequest
+                                    }}
+                                    // Optional props
+                                    onNoResult={this.handleNoResult}
+                                    onSelectSuggest={this.handleSelectSuggest}
+                                    textNoResults="My custom no results text" // null or "" if you want to disable the no results item
+                                    customRender={prediction => (
+                                        <div className="customWrapper input-group">
+                                            {prediction
+                                                ? prediction.description
+                                                : "My custom no results text"}
+                                        </div>
+                                    )}
+                                >
+                                    <input
+                                        className="form-control width100"
+                                        type="text"
+                                        value={value}
+                                        placeholder="Search a location"
+                                        onChange={this.handleInputChange}
+                                    />
+                                </ReactGooglePlacesSuggest>
+                                <span className="input-group-btn">
+                                    <button type="button" className="btn btn-warning" onClick={this.postAddress}>Save</button>
+                                    {/* <button type="button" className="btn btn-info">Select from Profile</button> */}
+                                    {this.state.loggedIn ? (
+                                    <select onChange={this.selectAddress} name="saved">
+                                        {this.state.userAddress.map(address => {
+                                            return (<option value={address}>{address}</option>)
+                                        })}
+                                    </select>
+                                    ) : (<></>)}
+                                </span>
+                            </div>
+                        </div>
                     )
                 }
-                
             />
         )
     }
