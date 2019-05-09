23e97ed3b3fb85feccb88680869243c498a08fd5
haleylanebrown <47645417+haleylanebrown@users.noreply.github.com>
2019-05-09 17:46:42 -0600

    fixing buttons

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ import "./style.css";
 function Footer() {
     return (
         <footer className="footer mt-auto py-3" id="Footer" style={{padding: "0px"}}>
-            <i className="fab fa-react react-icon"></i>Copywright© Jenn, Dylan, Haley, Kevin
+            <i className="fab fa-react react-icon"></i>Copyright© Jenn, Dylan, Haley, Kevin
             <a id="github" href="https://github.com/ksknight85/MeetInTheMiddle" target="blank"><i className="fab fa-github gh-icon"></i></a>
         </footer>
     )
